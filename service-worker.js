7f426eb07fad6dacfa672ef97aa3decd203c9105
Your Name <you@example.com>
2024-07-14 21:38:32 +0700

    install upade

--- service-worker.js
+++ service-worker.js
@@ -1,24 +1,40 @@
+const CACHE_NAME = 'idkdoro-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/script.js',
+  '/icon-192x192.png',
+  '/icon-512x512.png'
+];
+
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('pomodoro-timer').then(cache => {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          '/styles.css',
-          '/script.js',
-          '/manifest.json',
-          '/icon-192.png',
-          '/icon-512.png'
-        ]);
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
       })
-    );
-  });
-  
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then(response => {
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
         return response || fetch(event.request);
       })
-    );
-  });
-  
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
